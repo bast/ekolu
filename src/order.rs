029444bd07d7b4542f459ef08a4c43f5af1e5afb
Radovan Bast <bast@users.noreply.github.com>
2023-01-09 21:50:24 +0100

    functions to order tuples and triples

---
+++ src/order.rs
@@ -0,0 +1,17 @@
+fn order_tuple(i: usize, j: usize) -> (usize, usize) {
+    if i <= j {
+        (i, j)
+    } else {
+        (j, i)
+    }
+}
+
+fn order_triple(i: usize, j: usize, k: usize) -> (usize, usize, usize) {
+    let (a, b, c) = (i, j, k);
+
+    let (a, b) = order_tuple(a, b);
+    let (b, c) = order_tuple(b, c);
+    let (a, b) = order_tuple(a, b);
+
+    (a, b, c)
+}
